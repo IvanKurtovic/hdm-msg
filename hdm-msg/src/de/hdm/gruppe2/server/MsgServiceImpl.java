8008d2cc429c189982fcf1e32470d67a90b4045b
marina <mi008@hdm-stuttgart.de>
2015-11-17 09:30:51 +0100

    leeres Projekt, Service Klassen benannt, Business Object Klasse

---
+++ hdm-msg/src/de/hdm/gruppe2/server/MsgServiceImpl.java
@@ -0,0 +1,15 @@
+package de.hdm.gruppe2.server;
+
+import de.hdm.gruppe2.client.MsgService;
+import de.hdm.gruppe2.shared.FieldVerifier;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server-side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class MsgServiceImpl extends RemoteServiceServlet implements
+		MsgService {
+
+	
+}
