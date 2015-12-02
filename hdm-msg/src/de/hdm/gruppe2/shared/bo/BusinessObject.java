f92c29f02d9caefc3b32d18c6a0369f626bda3a8
IvanKurtovic <ik030@hdm-stuttgart.de>
2015-12-02 18:52:21 +0100

    Erstellung der Klasse User mit allen nötigen Operationen welche von der abstrakten Klasse BusinessObjekt abweichen

---
+++ hdm-msg/src/de/hdm/gruppe2/shared/bo/BusinessObject.java
@@ -0,0 +1,49 @@
+package de.hdm.gruppe2.shared.bo;
+
+import java.io.Serializable;
+import java.util.Date;
+/**
+ * <p>
+ * Die Klasse <code>BusinessObject</code> stellt die Basisklasse aller in diesem
+ * Projekt für die Umsetzung des Fachkonzepts relevanten Klassen dar.
+ * </p>
+ * <p>
+ * Zentrales Merkmal ist, dass jedes <code>BusinessObject</code> eine Nummer
+ * besitzt, die man in einer relationalen Datenbank auch als Primärschlüssel
+ * bezeichnen würde. Fernen ist jedes <code>BusinessObject</code> als
+ * {@link Serializable} gekennzeichnet. Durch diese Eigenschaft kann jedes
+ * <code>BusinessObject</code> automatisch in eine textuelle Form überführt und
+ * z.B. zwischen Client und Server transportiert werden. Bei GWT RPC ist diese
+ * textuelle Notation in JSON (siehe http://www.json.org/) kodiert.
+ * </p>
+ * 
+ * @author Thies & Ioannidou
+ */
+
+public abstract class BusinessObject implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Eindeutige ID für alle Businessobjekte.
+	 */
+	private int id;
+	private Date creationDate;
+
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
