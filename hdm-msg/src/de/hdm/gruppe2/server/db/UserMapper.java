63e45ef60b9e2cbf3af69dacd09e0ab4a7b8be58
serkan-yilmaz <sy007@hdm-stuttgart.de>
2015-12-22 13:24:10 +0100

    Erstellung des User-Mappers mit

---
+++ hdm-msg/src/de/hdm/gruppe2/server/db/UserMapper.java
@@ -0,0 +1,139 @@
+package de.hdm.gruppe2.server.db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.sql.PreparedStatement;
+
+import de.hdm.gruppe2.shared.bo.*;
+
+/**
+ * 
+ * 
+ * @author Thies
+ * 
+ * 
+ * 
+ */
+public class UserMapper {
+
+	
+	 
+	private static UserMapper userMapper = null;
+
+	
+	protected UserMapper() {
+
+	}
+
+	
+	public static UserMapper usermapper() {
+		if (userMapper == null) {
+			userMapper = new UserMapper();
+		}
+		return userMapper;
+	}
+
+	public User insert(User user) throws IllegalArgumentException {
+		// DB-Verbindung herstellen
+		Connection con = DBConnection.connection();
+		try {
+			// Insert-Statement erzeugen
+			// Statement stmt = con.createStatement();
+			// Zun�chst wird geschaut welches der momentan h�chste
+			// Prim�rschl�ssel ist
+			// ResultSet
+			// rs=stmt.executeQuery("SELECT MAX(UserID) AS maxid "+"FROM user");
+
+			// Wenn ein Datensatz gefunden wurde, wird auf diesen zugegriffen
+			// if(rs.next()) @Author: Thies{
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+			Date d = new Date();
+			try {
+				d = sdf.parse("21/12/2015");
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//user.setCreationDate(creationDate);
+
+			String sql = "INSERT INTO `user`(`GoogleID`, `vorname`, `nachname`, `email`, `nickname`, `datum`) "
+					+ "VALUES (NULL, ?, ?, ?, ?, ?)";
+
+			PreparedStatement preStmt;
+			preStmt = con.prepareStatement(sql);
+			preStmt.setString(1, user.getFirstName());
+			preStmt.setString(3, user.getLastName());
+			preStmt.setString(2, user.getEmail());
+			preStmt.setString(4, user.getGoogleId());
+			preStmt.executeUpdate();
+			preStmt.close();
+
+		
+	
+
+			
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+			throw new IllegalArgumentException("Datenbank fehler!"
+					+ e.toString());
+		}
+		return user;
+	}
+
+	public User update(User user) throws IllegalArgumentException {
+		Connection con = DBConnection.connection();
+		try {
+			PreparedStatement preStmt;
+			preStmt = con
+					.prepareStatement("UPDATE nutzer SET firstName=?, "
+							+ "LastName=?, email=?, date=? WHERE GoogleID="
+							+ user.getGoogleId());
+			preStmt.setString(1, user.getFirstName());
+			preStmt.setString(2, user.getLastName());
+			preStmt.setString(3, user.getEmail());
+			preStmt.setString(5, user.getCreationDate().toString());
+			preStmt.executeUpdate();
+			preStmt.close();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Datenbank fehler!"
+					+ e.toString());
+		}
+		return user;
+	}
+
+	/**
+	 * L�schen der Daten eines <code>User</code>-Objekts aus der
+	 * Datenbank.
+	 * 
+	 * @param a
+	 *
+	 *            das aus der DB zu l�schende "Objekt"
+	 *            @Autor Thies
+	 */
+	public void delete(User user) throws IllegalArgumentException {
+		Connection con = DBConnection.connection();
+		try {
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate("DELETE FROM user WHERE userID="
+					+ user.getId());
+			stmt.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Datenbank fehler!"
+					+ e.toString());
+		}
+	}
+
+}
