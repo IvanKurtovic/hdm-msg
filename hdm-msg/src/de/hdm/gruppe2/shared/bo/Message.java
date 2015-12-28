0026f32d9676de28740d59a66be8fa7675873336
CemKorkmaz <Cem.Korkmaz89@googlemail.com>
2015-12-28 12:13:43 +0100

    Alle Änderungen auf dem gleichen Stand.

---
+++ hdm-msg/src/de/hdm/gruppe2/shared/bo/Message.java
@@ -0,0 +1,50 @@
+package de.hdm.gruppe2.shared.bo;
+
+import java.util.ArrayList;
+
+public class Message extends BusinessObject {
+	
+	
+	private static long serialVersionUID = 1L;
+	
+	private String text; 
+	
+	private User sender; 
+	
+	private ArrayList<Hashtag> hashtagList;
+	
+	
+	private ArrayList<User> receiver;   
+	
+	public String getText() {
+		return text;
+	}
+
+	public void setText (String text) {
+		this.text = text; 
+	}
+	public User getUser() {
+		return sender;
+	}
+
+	public void setUser (User sender) {
+		this.sender = sender; 
+	}
+
+	public ArrayList<User> getReceiver() {
+		return this.receiver;
+	}
+	
+	
+	
+	public void setReciver (ArrayList<User> receiver) {
+		this.receiver = receiver; 
+	}
+	
+	public ArrayList<Hashtag> getHashtagList() {
+		return hashtagList; 
+	}
+	public void setHashtagList (ArrayList<Hashtag> hashtagList) {
+		this.hashtagList = hashtagList; 
+	}
+}
