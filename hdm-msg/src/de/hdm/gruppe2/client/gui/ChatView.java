71e008a6e9567be8a20a8f509168bf0abd6691dc
IvanKurtovic <ik030@hdm-stuttgart.de>
2015-12-29 20:33:17 +0100

    ChatView Klasse wurde angelegt---> Frontend Klasse

---
+++ hdm-msg/src/de/hdm/gruppe2/client/gui/ChatView.java
@@ -0,0 +1,13 @@
+package de.hdm.gruppe2.client.gui;
+
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class ChatView extends VerticalPanel {
+
+	
+	HTML welcomeText = new HTML("<h1>Wilkommen in der ChatView!</h1>");
+
+	
+	
+}
