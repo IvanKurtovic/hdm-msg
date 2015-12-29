cda04275c254439e5feef38d69cf3f4cb7cdcb8e
IvanKurtovic <ik030@hdm-stuttgart.de>
2015-12-29 20:35:31 +0100

    Login Relevante Klasse wurde angelegt aber Auskommentiert Muss am Ende beachtet werden

---
+++ hdm-msg/src/de/hdm/gruppe2/client/gui/Login.java
@@ -0,0 +1,11 @@
+package de.hdm.gruppe2.client.gui;
+
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class Login extends VerticalPanel {
+
+	HTML welcomeText = new HTML("<h1>Wilkommen in der Login View </h1>");
+
+	
+}
