0a7e033bc1bb954af2a0c62e11fe687655d73471
IvanKurtovic <ik030@hdm-stuttgart.de>
2015-12-29 20:32:38 +0100

    Klasse Abo --> Vertical Panel wurde angelegt---> Frontend Klasse

---
+++ hdm-msg/src/de/hdm/gruppe2/client/gui/AboView.java
@@ -0,0 +1,11 @@
+package de.hdm.gruppe2.client.gui;
+
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class AboView extends VerticalPanel {
+	
+	HTML welcomeText = new HTML("<h1>Wilkommen in der AboView!</h1>");
+
+
+}
