ff5f4acd86653d4443696225bea9218bc61e194f
SariKerim <ks146@hdm-stuttgart.de>
2015-12-29 14:07:33 +0100

    Menubar hinzugefügt und html und css file angepasst.

--- hdm-msg/src/de/hdm/gruppe2/client/HdmMsg.java
+++ hdm-msg/src/de/hdm/gruppe2/client/HdmMsg.java
@@ -1,8 +1,9 @@
 package de.hdm.gruppe2.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -13,18 +14,37 @@ public class HdmMsg extends VerticalPanel implements EntryPoint {
 	
 	public void onModuleLoad() {
 		
-	
-		Image welcomeImage = new Image();
-		HTML welcomeText = new HTML("<h1>Willkommen im Editor!</h1>");
+		Command test = new Command() {
+			public void execute() {
+				Window.alert("BANANAAAAA!!!");
+			}
+		};
 		
+		MenuBar messageMenu = new MenuBar(true);
+		messageMenu.addItem("Neue Nachricht", test);
 		
+		MenuBar userMenu = new MenuBar(true);
+		userMenu.addItem("Alle User anzeigen", test);
 		
-		//Hautpmenü schließlich dem RootPanel in den Menü-div Container zuordnen
+		MenuBar aboMenu = new MenuBar(true);
+		aboMenu.addItem("User Abos", test);
+		aboMenu.addItem("Hashtag Abos", test);
 		
-		RootPanel.get("content_wrap").add(welcomeImage);
-	    RootPanel.get("content_wrap").add(welcomeText);
-		RootPanel.get("Impressum").add(new Impressum());
+		MenuBar chatMenu = new MenuBar(true);
+		chatMenu.addItem("Chats anzeigen", test);
 		
-}
+		MenuBar mainMenu = new MenuBar();
+		mainMenu.setWidth("100%");
+		mainMenu.setAutoOpen(true);
+		mainMenu.addItem("Nachrichten", messageMenu);
+		mainMenu.addItem("Chats", chatMenu);
+		mainMenu.addItem("Users", userMenu);
+		mainMenu.addItem("Abos", aboMenu);
+		
+		//RootPanel.get("content_wrap").add(welcomeImage);
+	    RootPanel.get("header_wrap").add(mainMenu);
+		RootPanel.get("footer_wrap").add(new Impressum());
+		
+	}
 	
 }
