020bb37063a6990ac479c5230500044725de29b2
SariKerim <ks146@hdm-stuttgart.de>
2016-01-05 15:36:58 +0100

    UserOverview Klasse erstellt und hinzugefügt.

---
+++ hdm-msg/src/de/hdm/gruppe2/client/UserOverview.java
@@ -0,0 +1,111 @@
+package de.hdm.gruppe2.client;
+
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class UserOverview extends VerticalPanel{
+	
+	private String[] names = {"Kerim", "Serkan", "Cem", "Ivan", "Cemile", "Hatun",
+							  "Feridun", "Ibne", "Semsettin", "Bahtiyar", "Osman"};
+		
+	@Override
+	public void onLoad() {
+		
+		// User Details
+		final Grid mainGrid = new Grid(5,3);
+		final Grid detailsGrid = new Grid(4,2);
+		
+		final Label lblFirstName = new Label("Vorname: ");
+		final TextBox tbFirstName = new TextBox();
+		final Label lblLastName = new Label("Nachname: ");
+		final TextBox tbLastName = new TextBox();
+		final Label lblEmail = new Label("Email: ");
+		final TextBox tbEmail = new TextBox();
+		
+
+		// User List links
+		final ListBox userNames = new ListBox();
+		userNames.setStyleName("userNames");
+		userNames.setVisibleItemCount(11);
+		userNames.addChangeHandler(new ChangeHandler() {
+
+			@Override
+			public void onChange(ChangeEvent event) {
+				tbFirstName.setText(userNames.getValue(userNames.getSelectedIndex()));
+			}
+		});
+		
+		for (String s : names) {
+			userNames.addItem(s);
+		}
+		
+		//final HorizontalPanel pnlFunctions = new HorizontalPanel();		
+		final Button btnCreateUser = new Button("Neuer User");
+		final Button btnSaveUser = new Button("Speichern");
+		btnSaveUser.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				if(userNames.getSelectedIndex() == -1) {
+					Window.alert("Kein User ausgewaehlt, Moruk!");
+					return;
+				}
+				
+				if(userNames.getValue(userNames.getSelectedIndex()) == tbFirstName.getText()) {
+					Window.alert("Du hast nix gemacht, du Spasst Alder!");
+					return;	
+				} else if (userNames.getValue(userNames.getSelectedIndex()) != tbFirstName.getText()) {
+					int index = userNames.getSelectedIndex();
+					userNames.removeItem(index);
+					userNames.insertItem(tbFirstName.getText(), index);
+				}
+			}
+			
+		});
+		
+		final Button btnDeleteUser = new Button("Entfernen");
+		btnDeleteUser.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				if(userNames.getSelectedIndex() == -1) {
+					Window.alert("Kein User ausgewaehlt, Moruk!");
+					return;
+				}
+				
+				userNames.removeItem(userNames.getSelectedIndex());
+			}
+		});
+		
+		HorizontalPanel pnlSaveAndDeleteButton = new HorizontalPanel();
+		pnlSaveAndDeleteButton.add(btnCreateUser);
+		pnlSaveAndDeleteButton.add(btnDeleteUser);
+
+		detailsGrid.setWidget(0, 0, lblFirstName);
+		detailsGrid.setWidget(0, 1, tbFirstName);
+		detailsGrid.setWidget(1, 0, lblLastName);
+		detailsGrid.setWidget(1, 1, tbLastName);
+		detailsGrid.setWidget(2, 0, lblEmail);
+		detailsGrid.setWidget(2, 1, tbEmail);
+		detailsGrid.setWidget(3, 0, btnSaveUser);
+		
+		mainGrid.setWidget(0, 0, userNames);
+		mainGrid.setWidget(0, 1, detailsGrid);
+		mainGrid.setWidget(1, 0, pnlSaveAndDeleteButton);
+
+		this.add(mainGrid);
+	}
+
+}
